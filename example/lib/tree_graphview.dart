227dcf62db1a91a33c49a0997ac92e554d2f865b
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-08-06 22:08:32 +0300

    update

--- example/lib/tree_graphview.dart
+++ example/lib/tree_graphview.dart
@@ -102,40 +102,72 @@ class _TreeViewPageState extends State<TreeViewPage> {
 
   Random r = Random();
 
-  createUncle(int a) {
-    var clicked_node = graph.getNodeAtPosition(a!-1);
-        var clicked_node_parent_id = clicked_node.getParentId;
-        print('clicked Node parent: ${clicked_node_parent_id}');
-        var clicked_node_parent_node = graph.getNodeAtPosition(clicked_node_parent_id-1);
-        Node uncle_node = Node.Id(graph.nodeCount() + 1);
+  createCousin(int uncle_node_id) {
+        int nodeCount = graph.nodeCount();
+
+        Node uncle_node = graph.getNodeAtPosition(uncle_node_id-1);
+
+        Node cousin_node = Node.Id(nodeCount + 1);
+        cousin_node.setDescription = 'Cousin';
+        cousin_node.setParentId = uncle_node.key?.value;
+        graph.addEdge(uncle_node, cousin_node);
+
+        Node son_node = Node.Id(nodeCount + 2);
+        son_node.setDescription = 'Son';
+        son_node.setParentId = cousin_node.key?.value;
+        graph.addEdge(cousin_node, son_node);
+
+        Node daughter_node = Node.Id(nodeCount + 3);
+        daughter_node.setDescription = 'Daugther';
+        daughter_node.setParentId = cousin_node.key?.value;
+        graph.addEdge(cousin_node, daughter_node);
+
+        setState(() {});
+  }
+
+  createUncle() {
+        int nodeCount = graph.nodeCount();
+
+        Node grandfather_node = graph.getNodeAtPosition(0);
+
+        Node uncle_node = Node.Id(nodeCount + 1);
         uncle_node.setDescription = 'Uncle';
-        uncle_node.setParentId = clicked_node_parent_id;
-        Node cousin1_node = Node.Id(graph.nodeCount() + 2);
-        cousin1_node.setDescription = 'Cousin';
-        cousin1_node.setParentId = graph.nodeCount() + 1;
-        Node cousin2_node = Node.Id(graph.nodeCount() + 3);
-        cousin2_node.setDescription = 'Cousin';
-        cousin2_node.setParentId = graph.nodeCount() + 1;
-        Node son1_node = Node.Id(graph.nodeCount() + 4);
-        son1_node.setDescription = 'Son';
-        son1_node.setParentId = graph.nodeCount() + 2;
-        Node daughter1_node = Node.Id(graph.nodeCount() + 5);
-        daughter1_node.setDescription = 'Daugther';
-        daughter1_node.setParentId = graph.nodeCount() + 2;
-        Node son2_node = Node.Id(graph.nodeCount() + 6);
-        son2_node.setDescription = 'Son';
-        son2_node.setParentId = graph.nodeCount() + 3;
-        Node daughter2_node = Node.Id(graph.nodeCount() + 7);
-        daughter2_node.setDescription = 'Daughter';
-        daughter2_node.setParentId = graph.nodeCount() + 3;
-        print(clicked_node);
-        graph.addEdge(clicked_node_parent_node, uncle_node);
-        graph.addEdge(uncle_node, cousin1_node);
-        graph.addEdge(uncle_node, cousin2_node);
-        graph.addEdge(cousin1_node, son1_node);
-        graph.addEdge(cousin1_node, daughter1_node);
-        graph.addEdge(cousin2_node, son2_node);
-        graph.addEdge(cousin2_node, daughter2_node);
+        uncle_node.setParentId = 1;
+        graph.addEdge(grandfather_node, uncle_node);
+
+        createCousin(nodeCount + 1);
+        createCousin(nodeCount + 1);
+
+        // Node cousin1_node = Node.Id(nodeCount + 2);
+        // cousin1_node.setDescription = 'Cousin';
+        // cousin1_node.setParentId = uncle_node.key?.value;
+        // graph.addEdge(uncle_node, cousin1_node);
+
+        // Node cousin2_node = Node.Id(nodeCount + 3);
+        // cousin2_node.setDescription = 'Cousin';
+        // cousin2_node.setParentId = uncle_node.key?.value;
+        // graph.addEdge(uncle_node, cousin2_node);
+
+        // Node son1_node = Node.Id(nodeCount + 4);
+        // son1_node.setDescription = 'Son';
+        // son1_node.setParentId = cousin1_node.key?.value;
+        // graph.addEdge(cousin1_node, son1_node);
+
+        // Node daughter1_node = Node.Id(nodeCount + 5);
+        // daughter1_node.setDescription = 'Daugther';
+        // daughter1_node.setParentId = cousin1_node.key?.value;
+        // graph.addEdge(cousin1_node, daughter1_node);
+
+        // Node son2_node = Node.Id(nodeCount + 6);
+        // son2_node.setDescription = 'Son';
+        // son2_node.setParentId = cousin2_node.key?.value;
+        // graph.addEdge(cousin2_node, son2_node);
+
+        // Node daughter2_node = Node.Id(nodeCount + 7);
+        // daughter2_node.setDescription = 'Daughter';
+        // daughter2_node.setParentId = cousin2_node.key?.value;
+        // graph.addEdge(cousin2_node, daughter2_node);
+
         setState(() {});
   }
 
@@ -143,17 +175,36 @@ class _TreeViewPageState extends State<TreeViewPage> {
     return InkWell(
       onTap: () {
         print('clicked Node: ${a}');
-        createUncle(a!);
+        if (graph.getNodeAtPosition(a!-1).getDescription == 'Uncle') createUncle();
+        else if (graph.getNodeAtPosition(a!-1).getDescription == 'Cousin') createCousin(graph.getNodeAtPosition(a!-1).getParentId);
       },
-      child: Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4),
-            boxShadow: [
-              BoxShadow(color: Colors.blue[100]!, spreadRadius: 1),
-            ],
+      child: Row(        
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              boxShadow: [
+                BoxShadow(color: Colors.blue[100]!, spreadRadius: 1),
+              ],
+            ),
+            child: Text('Node ${a}, ${graph.getNodeAtPosition(a!-1).getDescription}')
           ),
-          child: Text('Node ${a}, ${graph.getNodeAtPosition(a!-1).getDescription}')),
+          SizedBox(
+            width: 10
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              boxShadow: [
+                BoxShadow(color: Colors.blue[100]!, spreadRadius: 1),
+              ],
+            ),
+            child: Text('Wife')
+          )
+        ],
+      ),
     );
   }
 
@@ -166,54 +217,77 @@ class _TreeViewPageState extends State<TreeViewPage> {
     Node root_node = Node.Id(0);
     root_node.setDescription = 'Root';
     print(root_node.getDescription);
-    Node node1 = Node.Id(1);
-    node1.setDescription = 'Grandfather';
-    node1.setParentId = 0;
-    print(node1.getDescription);
-    Node node2 = Node.Id(2);
-    node2.setDescription = 'Father';
-    node2.setParentId = 1;
-    print(node2.getDescription);
-    Node node3 = Node.Id(3);
-    node3.setDescription = 'Uncle';
-    node3.setParentId = 1;
-    print(node3.getDescription);
-    Node node4 = Node.Id(4);
-    node4.setDescription = 'Me';
-    node4.setParentId = 2;
-    print(node4.getDescription);
-    Node node5 = Node.Id(5);
-    node5.setDescription = 'Son';
-    node5.setParentId = 4;
-    print(node5.getDescription);
-    Node node6 = Node.Id(6);
-    node6.setDescription = 'Daughter';
-    node6.setParentId = 4;
-    print(node6.getDescription);
-    Node node7 = Node.Id(7);
-    node7.setDescription = 'Cousin';
-    node7.setParentId = 3;
-    print(node7.getDescription);
-    Node node8 = Node.Id(8);
-    node8.setDescription = 'Cousin';
-    node8.setParentId = 3;
-    print(node8.getDescription);
-    Node node9 = Node.Id(9);
-    node9.setDescription = 'Son';
-    node9.setParentId = 7;
-    print(node9.getDescription);
-    Node node10 = Node.Id(10);
-    node10.setDescription = 'Daughter';
-    node10.setParentId = 7;
-    print(node10.getDescription);
-    Node node11 = Node.Id(11);
-    node11.setDescription = 'Son';
-    node11.setParentId = 8;
-    print(node11.getDescription);
-    Node node12 = Node.Id(12);
-    node12.setDescription = 'Daughter';
-    node12.setParentId = 8;
-    print(node12.getDescription);
+
+    Node grandfather_node = Node.Id(1);
+    grandfather_node.setDescription = 'Grandfather';
+    grandfather_node.setParentId = 0;
+
+    Node father_node = Node.Id(2);
+    father_node.setDescription = 'Father';
+    father_node.setParentId = 1;
+    graph.addEdge(grandfather_node, father_node);
+
+    Node me_node = Node.Id(3);
+    me_node.setDescription = 'Me';
+    me_node.setParentId = 2;
+    print(me_node.getDescription);
+    graph.addEdge(father_node, me_node);
+
+    Node son_node = Node.Id(4);
+    son_node.setDescription = 'Son';
+    son_node.setParentId = 3;
+    print(son_node.getDescription);
+    graph.addEdge(me_node, son_node);
+    
+    Node daughter_node = Node.Id(5);
+    daughter_node.setDescription = 'Daughter';
+    daughter_node.setParentId = 3;
+    print(daughter_node.getDescription);
+    graph.addEdge(me_node, daughter_node);
+
+    createUncle();
+
+    // Node node3 = Node.Id(3);
+    // node3.setDescription = 'Uncle';
+    // node3.setParentId = 1;
+    // print(node3.getDescription);
+
+    // Node node7 = Node.Id(7);
+    // node7.setDescription = 'Cousin';
+    // node7.setParentId = 3;
+    // print(node7.getDescription);
+    // Node node8 = Node.Id(8);
+    // node8.setDescription = 'Cousin';
+    // node8.setParentId = 3;
+    // print(node8.getDescription);
+    // Node node9 = Node.Id(9);
+    // node9.setDescription = 'Son';
+    // node9.setParentId = 7;
+    // print(node9.getDescription);
+    // Node node10 = Node.Id(10);
+    // node10.setDescription = 'Daughter';
+    // node10.setParentId = 7;
+    // print(node10.getDescription);
+    // Node node11 = Node.Id(11);
+    // node11.setDescription = 'Son';
+    // node11.setParentId = 8;
+    // print(node11.getDescription);
+    // Node node12 = Node.Id(12);
+    // node12.setDescription = 'Daughter';
+    // node12.setParentId = 8;
+    // print(node12.getDescription);
+    
+    // graph.addEdge(node1, node3);
+    
+
+    // graph.addEdge(node3, node7);
+    // graph.addEdge(node3, node8);
+    // graph.addEdge(node7, node9);
+    // graph.addEdge(node7, node10);
+    // graph.addEdge(node8, node11);
+    // graph.addEdge(node8, node12);
+//createUncle();
+
     // final node4 = Node.Id(4);
     // final node5 = Node.Id(5);
     // final node6 = Node.Id(6);
@@ -223,17 +297,6 @@ class _TreeViewPageState extends State<TreeViewPage> {
     // final node10 = Node.Id(10);
     // final node11 = Node.Id(11);
     // final node12 = Node.Id(12);
-    graph.addEdge(node1, node2);
-    graph.addEdge(node1, node3);
-    graph.addEdge(node2, node4);
-    graph.addEdge(node4, node5);
-    graph.addEdge(node4, node6);
-    graph.addEdge(node3, node7);
-    graph.addEdge(node3, node8);
-    graph.addEdge(node7, node9);
-    graph.addEdge(node7, node10);
-    graph.addEdge(node8, node11);
-    graph.addEdge(node8, node12);
     //graph.addEdge(node1, node3, paint: Paint()..color = Colors.red);
     // graph.addEdge(node1, node4, paint: Paint()..color = Colors.blue);
     // graph.addEdge(node2, node5);
